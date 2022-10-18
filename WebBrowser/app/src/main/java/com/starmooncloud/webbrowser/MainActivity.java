1637667f56c1c0a8f29dd8c1b85d13818a90c60d
star-moon-cloud-k <kspecial44@gmail.com>
2022-10-18 10:10:25 +0900

    Android

---
+++ WebBrowser/app/src/main/java/com/starmooncloud/webbrowser/MainActivity.java
@@ -0,0 +1,61 @@
+package com.starmooncloud.webbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity {
+
+    EditText editUrl;
+    Button btnGo;
+    Button btnBack;
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editUrl= (EditText) findViewById(R.id.edtUrl);
+
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        web = (WebView) findViewById(R.id.webView1);
+
+        class CookWebViewClient extends WebViewClient{
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view , String url){
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+
+        }
+        web.setWebViewClient(new CookWebViewClient());
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.loadUrl(editUrl.getText().toString());
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.goBack();
+            }
+        });
+
+
+
+    }
+}
