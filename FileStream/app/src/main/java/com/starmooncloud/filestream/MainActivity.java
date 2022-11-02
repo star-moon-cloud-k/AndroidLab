6ee8bc75e0dbf3fd4016a8a2cbbf09b7b46da1e3
star-moon-cloud-k <kspecial44@gmail.com>
2022-11-02 09:04:47 +0900

    fileReadWrite

---
+++ FileStream/app/src/main/java/com/starmooncloud/filestream/MainActivity.java
@@ -0,0 +1,56 @@
+package com.starmooncloud.filestream;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnRead, btnWrite;
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnWrite = (Button) findViewById(R.id.btnWrite);
+
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    FileOutputStream outFs = openFileOutput("file.txt", Context.MODE_PRIVATE);
+                    String str = "쿡북 안드로이드";
+                    outFs.write(str.getBytes());
+                    outFs.close();
+                    Toast.makeText(getApplicationContext(), "File.txt가 생성됨" , Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                }
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    FileInputStream inFs = openFileInput("file.txt");
+                    byte[] txt = new byte[30];
+                    inFs.read(txt);
+                    String str = new String(txt);
+                    Toast.makeText(getApplicationContext(), str , Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "파일 없음", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
